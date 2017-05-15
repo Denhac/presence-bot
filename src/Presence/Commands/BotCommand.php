75143a958f720edb6d61154a2595fae82c27b525
Mark Vaughn <iftrueelsefalse@gmail.com>
2017-05-15 15:46:37 -0600

    V1

--- src/Presence/Commands/BotCommand.php
+++ src/Presence/Commands/BotCommand.php
@@ -1,14 +1,5 @@
 <?php
 
-/**
- * Copyright 2014-2016, SellerLabs <snagshout-devs@sellerlabs.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- *
- * This file is part of the Snagshout package
- */
-
 namespace Presence\Commands;
 
 use PhpSlackBot\Bot;
